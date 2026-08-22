95ea1bd8cecfdc3337f127c015989d7ee34a3d9c
zain <zainwani553@gmail.com>
2026-08-22 23:36:25 +0530

    learned object creation

--- javascript/objects/script.js
+++ javascript/objects/script.js
@@ -1,1 +1,51 @@
 console.log("hello from objects script file")
+// objects are the key value pair
+
+
+//when we have one person and that have multiple properties like
+// a student is having multiple things name,class ,stream, id, contactno.
+
+let users = {
+
+  user1 : {
+    id : 101,
+    name: "xv",
+    class : "12th",
+    result : "pass"
+},
+  user2 : {
+    id : 101,
+    name: "xv",
+    class : "12th",
+    result : "pass"
+}
+,
+  user3 : {
+    id : 101,
+    name: "xv",
+    class : "12th",
+    result : "pass"
+}
+
+}
+// this is the syntex of object it carries the key and values 
+
+console.log(users)
+
+// if(student.result == "pass"){
+
+//     console.log("this person is pass whose name is " + student.name)
+// }else{
+//    console.log( student.name  + "is fail")
+// }
+
+
+let userdetails = {
+
+    id : 502,
+    name : "major",
+    school :"Army goodwill"
+}
+
+
+console.log(userdetails)
