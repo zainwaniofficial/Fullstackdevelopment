4d2ae8aa511f838b4b29c37d203d7fafc2078996
zain <zainwani553@gmail.com>
2026-08-25 20:24:07 +0530

    hoisting from now

---
+++ javascript/hoisting/script.js
@@ -0,0 +1,23 @@
+// alert("hello from hoisting")
+console.log("Hoisting")
+
+
+//
+
+//if we access both the variable as as function before creation 
+
+console.log(a)
+
+greet();
+
+
+//when we run them we will see that the var will give only undefined but the function will run normally it is because when we see memory context program we know that when we create a var that is stored as undefined but the function gets its value firsly.....
+
+
+var a  = 25;
+
+function greet()  {
+
+    console.log("hello dosto.... kya aap ne check kiya ye ")
+}
+
