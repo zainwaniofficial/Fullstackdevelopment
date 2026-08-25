c3428173b8079c69a0d219302922aedc19d2f195
zain <zainwani553@gmail.com>
2026-08-25 23:51:02 +0530

    shadowing n functional scope

--- javascript/hoisting/script.js
+++ javascript/hoisting/script.js
@@ -6,7 +6,7 @@ console.log("Hoisting")
 
 //if we access both the variable as as function before creation 
 
-console.log(a)
+// console.log(a)
 
 greet();
 
@@ -14,11 +14,11 @@ greet();
 //when we run them we will see that the var will give only undefined but the function will run normally it is because when we see memory context program we know that when we create a var that is stored as undefined but the function gets its value firsly.....
 
 
-var a  = 25;
+// var a  = 25;
 
 function greet()  {
 
-    console.log("hello dosto.... kya aap ne check kiya ye ")
+    // console.log("hello dosto.... kya aap ne check kiya ye ")
 }
 
 
@@ -33,7 +33,7 @@ function greet()  {
     {
     {
     {
-    var  a = 20; 
+    // var  a = 20; 
 }
 
 
@@ -45,8 +45,87 @@ function greet()  {
 let greeting = () =>{
 
 
-     console.log(a)
+    //  console.log(a)
 }
 
 
 greeting()
+
+
+//function scope
+
+//variable inside the function can not be accessed outside 
+//lets check
+
+let fun = ()=>{
+
+    var a = 10;
+}
+
+fun()
+
+//if we try to access variable a outside it will give error 
+
+
+// console.log(a)
+
+
+//Global scope : har jagah se hum isko read kar sakte hai 
+
+//function scope : jo sirf function ke andar he hum access kar sakte hai
+ 
+
+// let is a block scoped means it cant be accessed outside the scope 
+
+
+{
+
+    let hello = "world"
+}
+
+
+//if we try to access this hello variable here we cant 
+
+//lets try 
+
+
+// console.log(hello)   it will give hello is not defined 
+
+
+var a = 50;
+
+function fun1(){
+        var a = 60;
+
+        console.log(a)
+}
+
+function fun2(){
+
+    var a = 80;
+
+    console.log(a)
+
+    fun1()
+}
+
+
+fun2()
+
+//shadowing
+
+//shadowing means to add one more same name variable in a local scope to avoid global scope
+
+
+let num = 50;
+
+
+let numbers = ()=>{
+
+    let num = 600; //this num here is called shadowing
+
+    console.log(num)
+}
+
+
+numbers()
