b17051c71d702c4ea558e7d795c24a56d11263e2
zain <zainwani553@gmail.com>
2026-08-26 22:22:55 +0530

    starting closures

---
+++ javascript/closures/script.js
@@ -0,0 +1,51 @@
+console.log("hello from closures")
+
+
+//closure means closest to the nearest scope
+
+
+// lets understand first the lexical scope
+
+//lexical scope ka matlab hai ke jaha function bana hai waha wo find karei ga cheezu ko na ke jaha hum usko call karte hai 
+
+//example 
+
+
+let a = 80;
+
+function one(){
+
+    console.log(a)
+}
+
+function two(){
+
+    let a = 600;
+    one()
+}
+
+
+two()
+
+
+//if we see code snippit ☝ here 
+
+//we will see that we will get the 80 answer because the function is created before function two no matter its is called near the variable where the value is 600 
+
+//this is the lexical scope
+ 
+
+//function jaha create huwa hai wo matter karta hai na ke jaha call kiya hai wahe hai lexical scope
+
+
+
+
+//now we see the closure 
+
+
+//it also the concept
+
+function outer (){
+
+    console.log("outer function")
+}
