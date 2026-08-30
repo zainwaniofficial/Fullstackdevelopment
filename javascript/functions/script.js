111feb8cfc06e146b27c86f773f001cfaf672c03
zain <zainwani553@gmail.com>
2026-08-30 23:09:00 +0530

    writing of function

--- javascript/functions/script.js
+++ javascript/functions/script.js
@@ -1,1 +1,18 @@
+
 console.log("hello from functions")
+//simple writing program
+
+function greet(){
+    console.log("traditional function")
+}
+
+greet()
+
+
+//arrow function
+
+
+let fun = ()=>{
+
+    console.log("hello i am function")
+}
